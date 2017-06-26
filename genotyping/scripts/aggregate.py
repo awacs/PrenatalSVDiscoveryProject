6865f606ea0fe13efc03226558b07e535d06d6d9
Matt Stone <mstone@chgr.mgh.harvard.edu>
2017-06-26 19:00:17 -0400

    add baf and metadata to aggregation

--- genotyping/scripts/aggregate.py
+++ genotyping/scripts/aggregate.py
@@ -9,13 +9,12 @@
 """
 
 import argparse
-from collections import deque
+from collections import deque, defaultdict
 import numpy as np
+import scipy.stats as ss
 import pandas as pd
-
-
-def aggregate():
-    pass
+import pysam
+import svtools.utils as svu
 
 
 def process_rdtest(rdtest):
@@ -42,11 +41,21 @@ def process_rdtest(rdtest):
     rdtest.loc[rdtest.log_pval == np.inf, 'log_pval'] = maxp + 5
     rdtest.loc[rdtest.log_2ndMaxP == np.inf, 'log_2ndMaxP'] = max2p + 5
 
+    rdtest.log_pval = rdtest.log_pval.abs()
+    rdtest.log_2ndMaxP = rdtest.log_2ndMaxP.abs()
+
     return rdtest
 
 
 def process_srtest(srtest):
     metrics = 'log_pval called_median bg_median'.split()
+
+    # remove -0.0 (temporary, should fix in SR-test)
+    srtest.log_pval = srtest.log_pval.abs()
+
+    # force one-sided (temporary, should fix in SR-test)
+    srtest.loc[srtest.bg_median > srtest.called_median, 'log_pval'] = 0
+
     srtest = srtest.pivot_table(index='name', values=metrics, columns='coord')
     srtest.columns = ['_'.join(col[::-1]).strip()
                       for col in srtest.columns.values]
@@ -55,13 +64,118 @@ def process_srtest(srtest):
     return srtest
 
 
+def process_petest(petest):
+    # remove -0.0 (temporary, should fix in PE-test)
+    petest.log_pval = petest.log_pval.abs()
+
+    # force one-sided (temporary, should fix in PE-test)
+    petest.loc[petest.bg_median > petest.called_median, 'log_pval'] = 0
+
+    return petest
+
+
+def process_baftest(baftest):
+    skip_cols = 'chrom start end samples svtype'.split()
+    baftest = baftest.drop(skip_cols, axis=1)
+
+    baftest['KS_log_pval'] = (- np.log10(baftest.KS_pval)).abs()
+
+    repl = 'Potential ROHregion or reference error'
+    baftest.delstat = baftest.delstat.replace(repl, 'Ref_error')
+
+    return baftest
+
+
 def preprocess(df, dtype):
     if dtype == 'RD':
         return process_rdtest(df)
     elif dtype == 'SR':
         return process_srtest(df)
+    elif dtype == 'PE':
+        return process_petest(df)
+    elif dtype == 'BAF':
+        return process_baftest(df)
     else:
         return df
+
+
+def _is_parent(s):
+    return s.endswith('fa') or s.endswith('mo')
+
+
+def _is_child(s):
+    return s.endswith('p1') or s.endswith('s1')
+
+
+def get_inh_rate(called):
+    quads = defaultdict(list)
+
+    for sample in called:
+        quad, member = sample.split('.')
+        quads[quad].append(member)
+
+    n_called = len([s for s in called if _is_child(s)])
+
+    n_inh = 0
+    for quad, members in quads.items():
+        if 'fa' in members or 'mo' in members:
+            if 'p1' in members:
+                n_inh += 1
+            if 's1' in members:
+                n_inh += 1
+
+    return n_inh / n_called
+
+
+def process_metadata(vcf):
+    samples = list(vcf.header.samples)
+    parents = [s for s in samples if _is_parent(s)]
+    children = [s for s in samples if _is_child(s)]
+    n_parents = len(parents)
+    n_children = len(children)
+
+    metadata = deque()
+    for record in vcf:
+        called = svu.get_called_samples(record)
+
+        # Calculate parental VF
+        parents = [s for s in called if _is_parent(s)]
+        parental_vf = len(parents) / n_parents
+
+        children = [s for s in called if _is_child(s)]
+        child_vf = len(children) / n_children
+
+        if child_vf > 0:
+            inh_rate = get_inh_rate(called)
+        else:
+            inh_rate = 0
+
+        dat = [record.id, record.info['SVTYPE'],
+               parental_vf, child_vf, inh_rate]
+        metadata.append(dat)
+
+    metadata = np.array(metadata)
+    cols = 'name svtype parental_vf child_vf inh_rate'.split()
+    metadata = pd.DataFrame(metadata, columns=cols)
+    return metadata
+
+
+def add_pesr(evidence):
+    evidence['PESR_called_median'] = (evidence['PE_called_median'] +
+                                      evidence['SR_sum_called_median'])
+    evidence['PESR_bg_median'] = (evidence['PE_bg_median'] +
+                                  evidence['SR_sum_bg_median'])
+
+    def calc_p(row):
+        pval = ss.poisson.cdf(row.PESR_bg_median, row.PESR_called_median)
+        return np.abs(-np.log10(pval))
+
+    evidence['PESR_log_pval'] = evidence.apply(calc_p, axis=1)
+
+    one_sided_mask = (evidence.PESR_bg_median > evidence.PESR_called_median)
+    evidence.loc[one_sided_mask, 'PESR_log_pval'] = 0
+
+    return evidence
 
 
 def main():
@@ -71,21 +185,38 @@ def main():
     parser.add_argument('-r', '--RDtest', required=True)
     parser.add_argument('-s', '--SRtest', required=True)
     parser.add_argument('-p', '--PEtest', required=True)
+    parser.add_argument('-b', '--BAFtest', required=True)
+
+    variants = parser.add_mutually_exclusive_group(required=True)
+    variants.add_argument('-d', '--bed')
+    variants.add_argument('-v', '--vcf')
     parser.add_argument('fout')
     args = parser.parse_args()
 
-    evidence = deque()
+    vcf = pysam.VariantFile(args.vcf)
+    metadata = process_metadata(vcf)
+    metadata = metadata.set_index('name')
 
-    for dtype in 'PE SR RD'.split():
-        df = pd.read_table(getattr(args, dtype + 'test'))
+    evidence = deque()
+    BAF_names = ('chrom start end name samples svtype delstat snp_ratio '
+                 'del_loglik dupstat KS_stat KS_pval total_case_snps '
+                 'total_snps n_nonROH_cases n_samples mean_control_snps '
+                 'n_nonROH_controls n_controls').split()
+
+    for dtype in 'PE SR RD BAF'.split():
+        names = BAF_names if dtype == 'BAF' else None
+        df = pd.read_table(getattr(args, dtype + 'test'), names=names)
+
         df = preprocess(df, dtype)
         df = df.rename(columns=lambda c: dtype + '_' + c if c != 'name' else c)
         df = df.set_index('name')
         evidence.append(df)
 
     evidence = list(evidence)
-    evidence = evidence[0].join(evidence[1:], how='outer', sort=True)
-    evidence = evidence.reset_index()
+    evidence = metadata.join(evidence, how='outer', sort=True)
+    evidence = evidence.reset_index().rename(columns={'index': 'name'})
+
+    evidence = add_pesr(evidence)
 
     evidence.to_csv(args.fout, index=False, sep='\t', na_rep='NA')
 
